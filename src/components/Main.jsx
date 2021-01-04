32492c943b07cba7da69f4825d985ac7b293b429
Toni Räsänen <toni.rasanen@helsinki.fi>
2021-01-04 16:25:06 +0200

    init styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
   },
 });
 
-const Main = () => {
+const Main = () => { 
   return (
     <View style={styles.container}>
       <Text>Rate Repository App</Text>
+      <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
+      <Text fontWeight="bold" fontSize="subheading">Bold subheading
+      </Text>
+      <Text color="primary">Text with primary color</Text>
       <RepositoryList/>
     </View>
   );
